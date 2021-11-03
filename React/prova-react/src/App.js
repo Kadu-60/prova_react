969c8c6accb39765b9294140ab386cc2f475c3ed
kadu <carlos.miranda@unesp.br>
2021-11-03 10:35:08 -0300

    10:35

---
+++ React/prova-react/src/App.js
@@ -0,0 +1,14 @@
+import Footer from './components/template/Footer/Footer';
+import Header from './components/template/Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ React/prova-react/src/components/template/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer(props) {
+
+    return(
+        <>
+         <p>2020 Collective. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank"
+        className="text-primary">W3layouts.</a></p>
+        </>
+    )
+}
+
+export default Footer
